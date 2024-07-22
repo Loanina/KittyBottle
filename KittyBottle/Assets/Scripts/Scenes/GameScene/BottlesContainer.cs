73493f2d09c32616136d4403a39dfa7663a87870
Loanina <yhololeenko@mail.ru>
2024-07-22 15:06:45 +0300

    Spawn points, bottle prefab to sprites

--- KittyBottle/Assets/Scripts/Scenes/GameScene/BottlesContainer.cs
+++ KittyBottle/Assets/Scripts/Scenes/GameScene/BottlesContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -10,10 +11,12 @@ namespace Scenes.GameScene
         [SerializeField] private Bottle bottlePrefab;
         [SerializeField] private List<List<Color>> levelColors;
         [SerializeField] private BottlesController bottlesController;
+        [SerializeField] private Dictionary<int, List<Vector3>> layoutSettings;
         private List<Bottle> bottles;
-
+        
         private void CreateBottles()
         {
+            /*
             bottles = new List<Bottle>();
             foreach (var bottleColors in levelColors)
             {
@@ -22,13 +25,33 @@ namespace Scenes.GameScene
                 bottles.Add(bottle);
                 bottle.OnClickEvent += bottlesController.OnClickBottle;
             }
+            */
+            bottles = new List<Bottle>();
+            for (var i = 0; i < levelColors.Count; i++)
+            {
+                var position = transform.TransformPoint(layoutSettings[levelColors.Count][i]);
+                var bottle = Instantiate(bottlePrefab, position, new Quaternion(), transform);
+                bottle.Initialize(levelColors[i]);
+                bottles.Add(bottle);
+                bottle.OnClickEvent += bottlesController.OnClickBottle;
+                bottle.SetDefaultPosition();
+            }
         }
 
         private void Start()
         {
             CreateBottles();
+            /*
             LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
             bottles.ForEach(bottle => bottle.SetDefaultPosition());
+            */  
+        }
+
+        [Serializable]
+        private class LayoutParams
+        {
+            public Vector3 position;
+            public Vector3 scale;
         }
     }
 }
